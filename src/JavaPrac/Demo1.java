e9a0d7631121fcf9fdcab4e338f4d3edf8fac715
Azmain Anan <azmainanan1998@gmail.com>
2023-08-25 00:04:55 +0600

    second commit

---
+++ src/JavaPrac/Demo1.java
@@ -0,0 +1,11 @@
+package JavaPrac;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		Demo1 de = new Demo1();
+		
+	}
+
+}
--- src/JavaPrac/FirstProgram.java
+++ src/JavaPrac/FirstProgram.java
@@ -1,3 +1,4 @@
+package JavaPrac;
 
 public class FirstProgram {
 

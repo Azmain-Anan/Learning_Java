4da7c064c9e7abebc0eb169e8f3943a7ccb53166
Azmain Anan <azmainanan1998@gmail.com>
2023-08-26 22:11:54 +0600

    DTType

---
+++ src/JavaPrac/DTType.java
@@ -0,0 +1,23 @@
+package JavaPrac;
+
+public class DTType {
+
+	public static void main(String[] args) {
+
+
+		byte bt = 12;
+		short sht = 3212;
+		int num = 123456789;
+		long lg = 617L;
+		float myFloat = 0.9876f;
+		double myDauble = 1.8765d;
+		boolean bool = true;
+		char ch = 'a';
+		
+		DTType dtt = new DTType();
+		
+		String srt = new String();
+
+	}
+
+}

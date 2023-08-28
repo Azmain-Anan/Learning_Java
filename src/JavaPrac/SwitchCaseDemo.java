14a4f2a1a4f2283afe3af9cf018f5df185e0f333
Azmain Anan <azmainanan1998@gmail.com>
2023-08-28 11:56:30 +0600

    Switch

---
+++ src/JavaPrac/SwitchCaseDemo.java
@@ -0,0 +1,36 @@
+package JavaPrac;
+
+public class SwitchCaseDemo {
+
+	public static void main(String[] args) {
+		int day = 7;
+		switch (day) {
+		case 1:
+			System.out.println("Sa");
+			break;
+		case 2:
+			System.out.println("Su");
+			break;
+		case 3:
+			System.out.println("Mo");
+			break;
+		case 4:
+			System.out.println("Tu");
+			break;
+		case 5:
+			System.out.println("We");
+			break;
+		case 6:
+			System.out.println("Th");
+			break;
+		case 7:
+			System.out.println("Fr");
+			break;
+		default:
+			System.out.println("None");
+			break;
+		}
+
+	}
+
+}

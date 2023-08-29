9bed58680d20ddc8123f51beb0d792b28cb4b7e9
Azmain Anan <azmainanan1998@gmail.com>
2023-08-29 14:23:15 +0600

    single dimention

---
+++ src/JavaPrac/SingleDimenArray.java
@@ -0,0 +1,19 @@
+package JavaPrac;
+
+public class SingleDimenArray {
+
+	public static void main(String[] args) {
+		int[] myArray = new int[10];
+		myArray[0]=5;
+		myArray[1]=15;
+		myArray[2]=25;
+		myArray[3]=35;
+		myArray[4]=45;
+		myArray[5]=55;
+		myArray[6]=65;
+
+		System.out.println(myArray[0]);
+		System.out.println(myArray.length);
+	}
+
+}

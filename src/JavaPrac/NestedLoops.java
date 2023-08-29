33b4d2004dee3a8d61043a8fbffeaa73ab196e8c
Azmain Anan <azmainanan1998@gmail.com>
2023-08-29 12:13:36 +0600

    nested loops

---
+++ src/JavaPrac/NestedLoops.java
@@ -0,0 +1,19 @@
+package JavaPrac;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)
+		{
+			int j=1;
+			while(j<=5)
+			{
+				System.out.println("Value of i"+i+" "+ "Value of j"+j);
+				j++;
+			}
+		}
+		
+
+	}
+
+}

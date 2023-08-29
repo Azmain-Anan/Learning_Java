ba1665ca6291fbc621f0993dd9059e3517d1cfad
Azmain Anan <azmainanan1998@gmail.com>
2023-08-29 11:41:01 +0600

    break

---
+++ src/JavaPrac/BreakAndContinue.java
@@ -0,0 +1,14 @@
+package JavaPrac;
+
+public class BreakAndContinue {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			if(i==7)
+				break;
+			System.out.println(i);
+		}
+	}
+
+}

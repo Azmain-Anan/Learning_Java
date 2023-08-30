500d43c5f4eb606b8e055c6323a1ce95f3b972ae
Azmain Anan <azmainanan1998@gmail.com>
2023-08-30 11:43:27 +0600

    Method

---
+++ src/JavaPrac/MethodDemo.java
@@ -0,0 +1,21 @@
+package JavaPrac;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		MethodDemo mt = new MethodDemo();
+		mt.logIn();
+		mt.logOut();
+		
+	}
+
+	public void logIn()
+	{
+		System.out.println("Successfully LogIn");
+	}
+	
+	public void logOut()
+	{
+		System.out.println("Successfully LogOut");
+	}
+}

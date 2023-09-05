fb7a62337755496c6a32b6cce7cfabdc0b7926b0
Azmain Anan <azmainanan1998@gmail.com>
2023-09-05 20:47:28 +0600

    shape

---
+++ src/Abstract/Circle.java
@@ -0,0 +1,28 @@
+package Abstract;
+
+public class Circle extends Shape{
+	
+	public static void main(String[] args) {
+		Circle c = new Circle();
+		c.drawShape();
+		c.colorShape();
+		c.moveShape();
+		
+	}
+
+	@Override
+	public void drawShape() {
+		System.out.println("Drawing Circle");
+	}
+
+	@Override
+	public void colorShape() {
+		System.out.println("Coloring Circle");
+	}
+
+	@Override
+	public void moveShape() {
+		System.out.println("Moving Circle");
+	}
+
+}
---
+++ src/Abstract/Shape.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+public abstract class Shape {
+	
+	public abstract void drawShape();
+	
+	public abstract void colorShape();
+	
+	public abstract void moveShape();
+
+}

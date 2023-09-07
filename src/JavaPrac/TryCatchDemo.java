4bace68316344cfff8fad38688ae8dc1c868102e
Azmain Anan <azmainanan1998@gmail.com>
2023-09-07 22:59:53 +0600

    TryCAtch

---
+++ src/JavaPrac/TryCatchDemo.java
@@ -0,0 +1,26 @@
+package JavaPrac;
+
+public class TryCatchDemo {
+
+	public static void main(String[] args) {
+		
+		try{
+			int i =10;
+			System.out.println("Before division");
+			System.out.println(i/0);
+		}
+		catch(Throwable t){
+			System.out.println(t.getMessage());
+			System.out.println(t.getCause());
+			System.out.println(t.getStackTrace());
+			t.printStackTrace()	;
+		}
+		
+		finally {
+			System.out.println("This is the final block");
+		}
+		
+		System.out.println("After division");
+	}
+
+}

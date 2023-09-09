6fcc6ca491218c9460e925940c8a668344476819
Azmain Anan <azmainanan1998@gmail.com>
2023-09-09 22:43:23 +0600

    bitm

---
+++ src/JavaPrac/bitmAssignment1.java
@@ -0,0 +1,20 @@
+package JavaPrac;
+
+public class bitmAssignment1 {
+
+	public static void main(String[] args) {
+		int i,j;
+		for (i=1;i<=3;i++) {
+			for(j=1;j<=3;j++)
+			{
+				System.out.print((i+j) +" ");
+				
+			}
+			System.out.println("=");
+			
+			
+		}
+
+	}
+
+}
